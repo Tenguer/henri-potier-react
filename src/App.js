3dc36e116fac8ae56b6856dbe76d39c3f6c2324c
andrew <andrew.obertan@icloud.com>
2021-02-26 10:54:31 +0100

    Use  saveBooks func on App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
-import React from 'react';
-import './App.scss';
-import NavBar from './components/NavBar/NavBar'
+import React, { useEffect }  from "react";
+import "./App.scss";
+import NavBar from "./components/NavBar/NavBar"
+
+
+import { getBooks } from "./services/bookService"
+import { saveBooks } from "./features/books/booksSlice"
+import { useDispatch } from "react-redux"
 
 function App() {
+  const dispatch = useDispatch()
+
+  // eslint-disable-next-line
+  useEffect(async () => {
+		try {
+			const response = await getBooks()
+      dispatch(saveBooks(response.data))
+		} catch (error) {
+			console.error(error)
+		}
+	}, [dispatch])
+
   return (
     <div className="App">
       <NavBar />
